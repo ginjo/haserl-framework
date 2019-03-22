443e9ea6093244df8e49406d14b46f30ca18008b
wbr <wbr@mac.com>
2019-03-22 16:46:09 -0700

    For safety, now redirecting all normal stdout from run() to &104, with all client-bound output going to &100 (which goes back to caller of run().

--- lib/framework.sh
+++ lib/framework.sh
@@ -27,6 +27,7 @@
 export SECRET="${SECRET:=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 128)}"
 export HF_DIRNAME="${HF_DIRNAME:=$(dirname $0)}"
 export HF_FRAMEWORK="${HF_FRAMEWORK:=$HF_DIRNAME/framework.sh}"
+export ROGUE_OUTPUT='104'
 
 . "$HF_DIRNAME/logging.sh"
 
@@ -36,18 +37,18 @@ set -a
 ##### User Functions #####
 
 before() {
-	log 5 "Defining 'before' function"
+  log 5 "Defining 'before' function"
   local code="$(cat -)"
   # Note that command-substitution $() strips ending newlines,
   # thus the EEOOLL and the subsequent brace-expansion to remove EEOOLL,
-	# yet keep the new lines.
+  # yet keep the new lines.
   run_before=$(printf '%s%s\n\nEEOOLL' "$run_before" "$code")
   run_before="${run_before%EEOOLL}"
   #printf 'BEFORE() called:%s' "$run_before" >&2
 }
 
 after() {
-	log 5 "Defining 'after' function"
+  log 5 "Defining 'after' function"
   local code="$(cat -)"
   run_after=$(printf '%s%s\n\nEEOOLL' "$run_after" "$code")
   run_after="${run_after%EEOOLL}"
@@ -57,7 +58,7 @@ route() {
   local match="$1"
   local method="$2"
   local code="$(cat -)"
-	log 5 '-echo "Defining route $match $method"'
+  log 5 '-echo "Defining route $match $method"'
   #set -a
   
   eval "action_match_$action_index='$match'"
@@ -72,14 +73,14 @@ route() {
 
 # Sets a new line in headers.
 header() {
-	log 6 "Setting header $1"
+  log 6 "Setting header $1"
   export headers=$(printf '%s\r\nEEOOLL' "$headers$1")
   headers="${headers%EEOOLL}"
 }
 
 content_type() {
   export content_type="$1"
-	log 6 '-echo "Set content type to $content_type"'
+  log 6 '-echo "Set content type to $content_type"'
 }
 
 # Redirects request.
@@ -90,17 +91,17 @@ content_type() {
 redirect() {
   location="$1"
   status="${2:-307 Temporary}"
-	log 4 '-echo "Redirecting  to $location, with status $status"'
-	#   printf '%s\r\n' "Status: $status"
-	#   printf '%s\r\n' "Location: $location"
-	# 	printf '%s\r\n' "Connection: Close"
-	#   printf '%s\r\n'
-	header "Status: $status"
-	header "Location: $location"
-	header "Connection: Close"
-	export redirected="$location"
-	headers
-}
+  log 4 '-echo "Redirecting  to $location, with status $status"'
+  #   printf '%s\r\n' "Status: $status"
+  #   printf '%s\r\n' "Location: $location"
+  #   printf '%s\r\n' "Connection: Close"
+  #   printf '%s\r\n'
+  header "Status: $status"
+  header "Location: $location"
+  header "Connection: Close"
+  export redirected="$location"
+  headers
+} >&100
 
 # This is main render, called from the app or controller.
 # Usage: render <view> <layout>
@@ -108,7 +109,7 @@ redirect() {
 render() {
   # Fork stubshell for each render() function, so current template,
   # which must be global, doesn't get confused when calling sub-render functions.
-	# TODO: Is this subshelling still necessary with new architecture.
+  # TODO: Is this subshelling still necessary with new architecture.
   (
     if [ ! -z "$1" ]; then
       export template="${1}"
@@ -116,7 +117,7 @@ render() {
   
     local layout="${2}"
 
-		log 5 '-echo "Rendering with $template $layout"'
+    log 5 '-echo "Rendering with $template $layout"'
 
     if [ ! -z "$layout" ]; then
       #export top_level_template="$template"
@@ -128,23 +129,23 @@ render() {
       echo "${REQUEST_BODY:-$POST_body}" | haserl "$APPDIR/views/$template"
     fi
   )
-}
+} >&100
 
 yield() {
-	log 5 '-echo "Yielding with $template"'
+  log 5 '-echo "Yielding with $template"'
   render "$template"
   #echo "Calling yield with top_level_template '$top_level_template'" >&2
   #render "$top_level_template"
-}
+} >&100
 
 # Return non-haserl text to client.
 # Make sure to set content_type appropriately.
 output() {
-	log 5 'Running output()'
+  log 5 'Running output()'
   local data="${1:-$(cat -)}"
   headers
   printf '%s\r\n' "$data"
-}
+} >&100
 
 # See https://stackoverflow.com/questions/12873682/short-way-to-escape-html-in-bash
 html_escape() {
@@ -162,7 +163,7 @@ html_escape() {
 set_cookie() {
   local name="$1"
   local data="$2"
-	log 6 '-echo "Called set_cookie with $name $data"'
+  log 6 '-echo "Called set_cookie with $name $data"'
   shift; shift;
   local enc_cookie_data=$(printf '%s' "$data" | encrypt)
   local cookie_params=$(for x in $@; do printf '; %s' "$x"; done)
@@ -173,7 +174,7 @@ set_cookie() {
 
 get_cookie() {
   local name="$1"
-	log 6 '-echo "Called get_cookie with $name"'
+  log 6 '-echo "Called get_cookie with $name"'
   eval "local raw=\$COOKIE_$name"
   if [ -z "$raw" ]; then return 1; fi
   printf '%s' "$raw" | decrypt
@@ -262,16 +263,16 @@ cookie_unsafe() {
 ##### Internal Setup & Server Functions #####
 
 setup() {
-	log 5 "Running setup()"
+  log 5 "Running setup()"
   if [ ! -z $is_setup ]; then
     return 0
   fi
   
-	# TODO: Should this be in server.sh, or stay here? Where is it used?
+  # TODO: Should this be in server.sh, or stay here? Where is it used?
   APPDIR="${APPDIR:=$( dirname "$(readlink -f "$0")" )}"
 
-	# Same here as above.
-	PUBLICDIR="${HF_PUBLIC:=${APPDIR}/public}"
+  # Same here as above.
+  PUBLICDIR="${HF_PUBLIC:=${APPDIR}/public}"
 
   action_index=1
   is_setup=true
@@ -280,75 +281,75 @@ setup() {
 # Runs the action after routes have been defined by user.
 # Expects request env vars to be populated already.
 #
-# NOTE:
-#   All stdout during the request 'run' should be sent to the fifo-output, and therefore back to the s/cgi.
-#   Use stderr during the request 'run' for all messages that should be sent back to server stdout or log.
+#	For safety, normal stdout of the run() funtion is redirected to $ROGUE_OUTPUT fd (defaults to '104').
+# All client-bound output during the request 'run' should be sent to &100.
+# Use stderr during the request 'run' for all messages that should be sent back to server stdout or log.
 #
 # Expects no input from stdin or from args.
 # Assumes all necessary data is in env vars.
 #
 run() {
-	log 5 "Called run()"
-  { 
-		# Experimental set PATH_INFO to '/' under certain circumstances.
-		if [ "$REQUEST_URI" = "$SCRIPT_NAME/" ]; then
-			export PATH_INFO='/'
-		fi
-    # This is needed for when path-info is empty or '/'.
-		# PATH_INFO will be null even if request is '/' (at least in openwrt).
-		# The above PATH_INFO modification may make this following bit obsolete.
-    local path_info="$PATH_INFO"
-    if [ -z "$path_info" ]; then
-      path_info='/'
-    fi
-		
-		# Serves static assets.
-		# NOTE: Experimental, this does not server the assets properly yet.
-		if [ -f "${PUBLICDIR}${path_info}" ]; then
-			log 4 '-echo "Serving static asset ${PUBLICDIR}${path_info}"'
-			header "Content-Type: application/octet-stream"
-			headers
-			cat "${PUBLICDIR}${path_info}"
-			return 0
-		fi
-    
-    # Selects matching PATH_INFO and REQUEST_METHOD (if request-method constraint is defined),
-    # then calls action associated with route.
-		# Any stdout here goes back to browser, but this loop doesn't generate any content iteself.
-    for i in $( seq 1 $(($action_index - 1)) ); do
-      eval "local match=\$action_match_$i"
-      eval "local code=\$action_code_$i"
-      eval "local method=\$action_method_$i"
-      #if [ "$match" == "$path_info" ] && [ "$method" == "$REQUEST_METHOD" -o -z "$method" ]; then
-			if [ "$method" == "$REQUEST_METHOD" -o -z "$method" ] && match_url "$path_info" "$match"; then
-        run_before
-        #echo "Test-error from just before (eval 'code') within run() function." >&2
-				if [ -z "$redirected" -a $? = 0 ]; then
-					log 6 "Running action with match ($match) method ($method) code ($code)"
-	        eval "$code" #2>>haserl_framework.log
-	        printf '\r\n'
-				fi
-        run_after >&2
-        return 0
+  log 5 "Called run()"
+  # Experimental set PATH_INFO to '/' under certain circumstances.
+  if [ "$REQUEST_URI" = "$SCRIPT_NAME/" ]; then
+    export PATH_INFO='/'
+  fi
+  
+  # This is needed for when path-info is empty or '/'.
+  # PATH_INFO will be null even if request is '/' (at least in openwrt).
+  # The above PATH_INFO modification may make this following bit obsolete.
+  local path_info="$PATH_INFO"
+  if [ -z "$path_info" ]; then
+    path_info='/'
+  fi
+  
+  # Serves static assets.
+  # NOTE: Experimental, this does not server the assets properly yet.
+  if [ -f "${PUBLICDIR}${path_info}" ]; then
+    log 4 '-echo "Serving static asset ${PUBLICDIR}${path_info}"'
+    header "Content-Type: application/octet-stream"
+    headers >&100
+    cat "${PUBLICDIR}${path_info}" >&100
+    return 0
+  fi
+   
+   # Selects matching PATH_INFO and REQUEST_METHOD (if request-method constraint is defined),
+   # then calls action associated with route.
+  # Any stdout here goes back to browser, but this loop doesn't generate any content iteself.
+   for i in $( seq 1 $(($action_index - 1)) ); do
+     eval "local match=\$action_match_$i"
+     eval "local code=\$action_code_$i"
+     eval "local method=\$action_method_$i"
+     #if [ "$match" == "$path_info" ] && [ "$method" == "$REQUEST_METHOD" -o -z "$method" ]; then
+    if [ "$method" == "$REQUEST_METHOD" -o -z "$method" ] && match_url "$path_info" "$match"; then
+       run_before >&2
+       #echo "Test-error from just before (eval 'code') within run() function." >&2
+      if [ -z "$redirected" -a $? = 0 ]; then
+        log 6 "Running action with match ($match) method ($method) code ($code)"
+        eval "$code"
+        printf '\r\n' >&100
       fi
-    done
+      #echo "Some rogue text in the run() function"
+      run_after >&2
+      return 0
+     fi
+   done
 
-    # If no path-info matches a defined route, output a generic response,
-    # and then return 1.
-    content_type 'text/plain'
-    #headers
-    #echo "Error: action failed"
-    #echo "$!"
-    output "haserl_framework: an error occurred, or no action matched $REQUEST_METHOD '$PATH_INFO'\
-    $1"
-    return 1
-  } #2>>haserl_framework.log
-}
+   # If no path-info matches a defined route, output a generic response,
+   # and then return 1.
+   content_type 'text/plain'
+   #headers
+   #echo "Error: action failed"
+   #echo "$!"
+   output "haserl_framework: an error occurred, or no action matched $REQUEST_METHOD '$PATH_INFO'\
+   $1"
+   return 1
+} 100>&1 1>&$ROGUE_OUTPUT
 
 run_before() {
   if [ ! -z "$run_before" ]; then
-		log 5 "Running 'before' actions"
-		log 6 "Before actions to run: $run_before"
+    log 5 "Running 'before' actions"
+    log 6 "Before actions to run: $run_before"
     #printf 'RUN_BEFORE:\n%s\nEND_RUN_BEFORE\n' "$run_before" >&2
     eval "$run_before"
   fi
@@ -356,8 +357,8 @@ run_before() {
 
 run_after() {
   if [ ! -z "$run_after" ]; then
-		log 5 "Running 'after' actions"
-		log 6 "After actions to run: $run_after"
+    log 5 "Running 'after' actions"
+    log 6 "After actions to run: $run_after"
     #printf 'RUN_AFTER:\n%s\nEND_RUN_AFTER\n' "$run_after" >&2
     eval "$run_after"
   fi
@@ -369,11 +370,11 @@ headers() {
   # According to RFC 2616, proper header-block termination should be \r\n\r\n,
   # and each header line should be terminated with \r\n.
   # printf 'HEADERS:\n%s\nEND_HEADERS\n\n' "$headers" >&2
-	header "Connection: close"
+  header "Connection: close"
   export headers=$(printf '%s\r\n%sEEOOLL' "Content-Type: ${content_type:-text/html}" "$headers")
-	if echo "$GATEWAY_INTERFACE" | grep -qv '^CGI' && [ ! "$SCGI" == '1' ]; then
-		headers=$( printf '%s\r\n%s' "HTTP/1.1 ${status:-200 OK}" "$headers")
-	fi
+  if echo "$GATEWAY_INTERFACE" | grep -qv '^CGI' && [ ! "$SCGI" == '1' ]; then
+    headers=$( printf '%s\r\n%s' "HTTP/1.1 ${status:-200 OK}" "$headers")
+  fi
   headers="${headers%EEOOLL}"
   #printf 'HEADERS:\n%s\nEND_HEADERS\n\n' "$headers" >&2
   printf '%s\r\n' "$headers"
@@ -392,51 +393,51 @@ get_safe_fifo_input() {
 # Matches PATH_INFO string with route definition,
 # creating variables from uri-inline-params if they exist.
 match_url() {  # <url> <matcher>
-	#echo "URL: $1, MATCHER: $2" >&2
-	
-	# Replaces var labels in url with general regexes.
-	# This is used to see if the URL matches the route in general.
-	gate_builder='s|:[[:alnum:]_]\{1,99\}|[^/]*|g'
-	#echo "GATE_BUILDER: $gate_builder" >&2
-	
-	# Builds pattern to match this label instance against URL.
-	local gate="$(echo $2 | sed -e $gate_builder )"
-	#echo "URL GATE: $gate" >&2
-	
-	# Matches URL against given route... or not.
-	if ! echo "$1" | grep -q "^$gate$"; then
-		return 1
-	# else
-	# 	echo "URL '$1' matched route '$2' with pattern '^$gate\$'" >&2
-	fi
+  #echo "URL: $1, MATCHER: $2" >&2
+  
+  # Replaces var labels in url with general regexes.
+  # This is used to see if the URL matches the route in general.
+  gate_builder='s|:[[:alnum:]_]\{1,99\}|[^/]*|g'
+  #echo "GATE_BUILDER: $gate_builder" >&2
+  
+  # Builds pattern to match this label instance against URL.
+  local gate="$(echo $2 | sed -e $gate_builder )"
+  #echo "URL GATE: $gate" >&2
+  
+  # Matches URL against given route... or not.
+  if ! echo "$1" | grep -q "^$gate$"; then
+    return 1
+  # else
+  #   echo "URL '$1' matched route '$2' with pattern '^$gate\$'" >&2
+  fi
 
-	# Picks out labels from url.
-	labels="$(echo $2 | grep -o ':\w*')"
-	#echo "$labels" >&2
-	
-	for x in $labels; do
-		# Builds pattern_builder to extract pattern-matcher for this label instance.
-		local pattern_builder="s|$x|\\\([^/]*\\\)|;s|:[[:alnum:]_]\{1,99\}|[^/]*|g"
-		#echo "PATTERN_BUILDER: $pattern_builder" >&2
-		
-		# Builds sed pattern to match this label instance against URL.
-		local pattern="s|$(echo $2 | sed -e $pattern_builder )|\\1|g"
-		#echo "PATTERN: $pattern" >&2
-		
-		# Extracts this label's param from URL.
-		result="$(echo $1 | sed -e $pattern)"
-		#echo "RESULT: $result" >&2
-		
-		# Assigns param value to var.
-		x="$(echo $x | tr -d ':')"
-		#echo "EVAL: PARAM_${x}=${result}" >&2
-		# NOTE: Very important to eval the $result var literally and
-		# not the expanded $result, or you will eval user input!
-		# That is why the single-quotes.
-		# Use this URL to test against malicious input: http://wndr3800/cgi-bin/proxy.cgi/order/"';$(ls);'bill/9|99;
-		#eval "PARAM_${x}="'${result}'
-		eval "PARAM_${x}=\""'${result}'\"
-	done
+  # Picks out labels from url.
+  labels="$(echo $2 | grep -o ':\w*')"
+  #echo "$labels" >&2
+  
+  for x in $labels; do
+    # Builds pattern_builder to extract pattern-matcher for this label instance.
+    local pattern_builder="s|$x|\\\([^/]*\\\)|;s|:[[:alnum:]_]\{1,99\}|[^/]*|g"
+    #echo "PATTERN_BUILDER: $pattern_builder" >&2
+    
+    # Builds sed pattern to match this label instance against URL.
+    local pattern="s|$(echo $2 | sed -e $pattern_builder )|\\1|g"
+    #echo "PATTERN: $pattern" >&2
+    
+    # Extracts this label's param from URL.
+    result="$(echo $1 | sed -e $pattern)"
+    #echo "RESULT: $result" >&2
+    
+    # Assigns param value to var.
+    x="$(echo $x | tr -d ':')"
+    #echo "EVAL: PARAM_${x}=${result}" >&2
+    # NOTE: Very important to eval the $result var literally and
+    # not the expanded $result, or you will eval user input!
+    # That is why the single-quotes.
+    # Use this URL to test against malicious input: http://wndr3800/cgi-bin/proxy.cgi/order/"';$(ls);'bill/9|99;
+    #eval "PARAM_${x}="'${result}'
+    eval "PARAM_${x}=\""'${result}'\"
+  done
 }
 
 
